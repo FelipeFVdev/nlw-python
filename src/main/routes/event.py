9fd751c11ad7232f409cfc019aa157b60d5017d1
FelipeFVdev <feliep8@gmail.com>
2025-02-23 22:31:19 -0300

    feat: implementing new routes

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, request
 
+event_route_bp = Blueprint("event_route", __name__)
+
 from src.validators.events_creator_validator import events_creator_validator
 
 from src.http_types.http_request import HttpRequest
 
 from src.controllers.events.events_creator import EventsCreator
 from src.model.repositories.eventos_repository import EventosRepository
-
-event_route_bp = Blueprint("event_route", __name__)
 
 
 @event_route_bp.route("/event", methods=["POST"])
@@ -18,6 +18,6 @@ def create_new_event():
     eventos_repo = EventosRepository()
     events_creator = EventsCreator(eventos_repo)
 
-    http_response = events_creator.create_event(http_request)
+    http_response = events_creator.create(http_request)
 
     return jsonify(http_response.body), http_response.status_code
